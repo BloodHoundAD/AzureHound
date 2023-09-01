32c61fef637eaf38ef810b528cb597a2ed7abef8
Dillon Lees <dlees@specterops.io>
2023-09-01 12:45:39 -0400

    chore: replace ioutil.readall with io.readall

--- client/rest/utils.go
+++ client/rest/utils.go
@@ -25,7 +25,6 @@ import (
 	"encoding/pem"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"strings"
 	"time"
 
@@ -36,7 +35,7 @@ import (
 
 func Decode(body io.ReadCloser, v interface{}) error {
 	defer body.Close()
-	defer ioutil.ReadAll(body)
+	defer io.ReadAll(body) // must read all; streaming to the json decoder does not read to EOF making the connection unavailable for reuse
 	return json.NewDecoder(body).Decode(v)
 }
 
